9fb788f50e292e6cbc0b79e5bdbdd6209ceb04de
Alatyami <alatyami@gmail.com>
2022-10-01 18:51:23 -0600

    Finished Apple Seed planting

--- src/main/java/growthcraft/apples/item/AppleSeedsItem.java
+++ src/main/java/growthcraft/apples/item/AppleSeedsItem.java
@@ -1,11 +1,17 @@
 package growthcraft.apples.item;
 
+import growthcraft.apples.init.GrowthcraftApplesBlocks;
 import growthcraft.lib.item.GrowthcraftItem;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.GrassBlock;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class AppleSeedsItem extends GrowthcraftItem {
 
@@ -14,9 +20,23 @@ public class AppleSeedsItem extends GrowthcraftItem {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level p_41432_, Player p_41433_, InteractionHand p_41434_) {
-        // TODO: Plant a AppleTreeSapling on use on the ground.
-
-        return super.use(p_41432_, p_41433_, p_41434_);
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel();
+        BlockPos blockpos = context.getClickedPos();
+        BlockState blockstate = level.getBlockState(blockpos);
+        Block targetBlock = blockstate.getBlock();
+        if (targetBlock != null && targetBlock instanceof GrassBlock) {
+            Player playerentity = context.getPlayer();
+            level.playSound(playerentity, blockpos, SoundEvents.CROP_PLANTED, SoundSource.BLOCKS, 1.0F, 1.0F);
+            if (!level.isClientSide) {
+                level.setBlock(blockpos.above(), GrowthcraftApplesBlocks.APPLE_TREE_SAPLING.get().defaultBlockState(), 11);
+                if (playerentity != null) {
+                    context.getItemInHand().shrink(1);
+                }
+            }
+            return InteractionResult.SUCCESS;
+        } else {
+            return InteractionResult.PASS;
+        }
     }
 }
